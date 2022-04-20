850c87c7e26344294eab4e5204ba64517cacff5e
Ian McPhail <ian@clerk.dev>
2022-04-20 13:26:31 -0400

    Add DB utils

---
+++ app/utils/db.server.js
@@ -0,0 +1,27 @@
+import { getAuth } from '@clerk/remix/ssr.server';
+import faunadb from 'faunadb';
+
+export const getClient = async (request) => {
+  const { userId, getToken } = await getAuth(request);
+
+  if (!userId) {
+    return null;
+  }
+
+  const secret = await getToken({ template: 'fauna' });
+
+  return new faunadb.Client({ secret });
+};
+
+export const q = faunadb.query;
+
+export const getUserById = async (id) => {
+  const response = await fetch(`https://api.clerk.dev/v1/users/${id}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.CLERK_API_KEY}`
+    }
+  });
+  const user = await response.json();
+
+  return user;
+};
